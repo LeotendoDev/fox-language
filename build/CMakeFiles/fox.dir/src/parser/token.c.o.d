b93a7cde943337d651fb48e6bbf5400ec80eaba3
Leonard Steinhoff <70445510+LeotendoDev@users.noreply.github.com>
2022-03-01 20:59:26 +0100

    🗑️ Delete binarys
    Delete binary from the "build" directory

--- build/CMakeFiles/fox.dir/src/parser/token.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fox.dir/src/parser/token.c.o: \
- /home/leonard/Desktop/coding/fox_lang/src/parser/token.c \
- /usr/include/stdc-predef.h \
- /home/leonard/Desktop/coding/fox_lang/src/parser/token.h
