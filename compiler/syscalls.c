91fb9e7fd7b27fb84a1a1169b4e69d14d5167219
LeotendoDev <leotendosteinhoff@gmail.com>
2022-03-02 20:41:05 +0100

    compiler/syscalls.c :: Implement linux syscall functions

---
+++ compiler/syscalls.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <ptrace.h>
+
+#include "syscalls.h"
+
+void call_syswrite(unsigned int buffer, const char* message, size_t length) 
+{
+
+}
+
+void call_sysfork(struct pt_regs target)
+{
+
+}
+
+void call_sysread(unsigned int buffer, char* message, size_t length)
+{
+
+}
+
+void call_sysclose(unsigned int buffer)
+{
+
+}
+
+void call_sysexit(int exit_code)
+{
+
+}
--- compiler/syscalls.h
+++ compiler/syscalls.h
@@ -1,6 +1,9 @@
 #ifndef _FOX_SYSCALLS_H_
 #define _FOX_SYSCALLS_H_
 
+#include <stdlib.h>
+#include <ptrace.h>
+
 #ifdef __linux__
 #define SYS_WRITE   0x1
 #define SYS_FORK    0x2
@@ -8,6 +11,13 @@
 #define SYS_WRITE   0x4
 #define SYS_OPEN    0x5
 #define SYS_CLOSE   0x6
+
+void call_syswrite(unsigned int buffer, const char* message, size_t length);
+void call_sysfork(struct pt_regs target);
+void call_sysread(unsigned int buffer, char* message, size_t length);
+void call_sysclose(unsigned int buffer);
+void call_sysexit(int exit_code);
+
 #endif
 
 #endif
