5c824211263441a77900afcdc9343490be7509de
Leonard Steinhoff <70445510+LeotendoDev@users.noreply.github.com>
2022-03-02 09:53:05 +0100

    parser/token.c :: Remove copyright sign #2

--- parser/token.c
+++ parser/token.c
@@ -1,11 +1,3 @@
-// SPDX-License Identifier: GPL-2.0-only
-/*
- * parser/token.c
- *
- * Copyright (C) 2022  Leonard Steinhoff
- *
-*/
-
 #include "token.h"
 
 const unsigned char* _fox_parser_tokens[18] = {
