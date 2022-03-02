7fbb2a1fe48c64b428079309ea61ec3e8c799731
LeotendoDev <leotendosteinhoff@gmail.com>
2022-03-02 18:12:05 +0100

    compiler/syscalls.h :: Add syscall list

---
+++ compiler/syscalls.h
@@ -0,0 +1,13 @@
+#ifndef _FOX_SYSCALLS_H_
+#define _FOX_SYSCALLS_H_
+
+#ifdef __linux__
+#define SYS_WRITE   0x1
+#define SYS_FORK    0x2
+#define SYS_READ    0x3
+#define SYS_WRITE   0x4
+#define SYS_OPEN    0x5
+#define SYS_CLOSE   0x6
+#endif
+
+#endif
