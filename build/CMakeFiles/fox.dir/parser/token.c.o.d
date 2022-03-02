6770266a9b1250507e30718c4f23fd8a75f5b71f
Leonard Steinhoff <70445510+LeotendoDev@users.noreply.github.com>
2022-03-02 18:15:49 +0100

    build/ :: Delete build directory

--- build/CMakeFiles/fox.dir/parser/token.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fox.dir/parser/token.c.o: \
- /home/leonard/Desktop/coding/fox-language/parser/token.c \
- /usr/include/stdc-predef.h \
- /home/leonard/Desktop/coding/fox-language/parser/token.h
